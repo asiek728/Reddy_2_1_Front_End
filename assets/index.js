cc28aad140b3de553c8502db72a04187b035c79e
“TatyanaA” <t.alchikova@gmail.com>
2023-11-28 14:14:56 +0000

    change link based on admin/user

--- assets/index.js
+++ assets/index.js
@@ -1,20 +1,27 @@
 console.log('hello');
 
-const is_ad=localStorage.getItem("isAdmin")
 
-console.log(is_ad)
 
 function changeHref() {
+    const is_ad=localStorage.getItem("isAdmin")
+
+   
+   
     
     const link = document.getElementById('link6');
-    if (is_ad==true) {
-        
+
+    console.log("link.href 13 " + link.href)
+    console.log("isAdmin line 8 " + is_ad)
+    if (is_ad === "true") {
+        console.log("isAdmin line 14 " + link.href)
 
     
     link.href = "./admin.html";  
     console.log("It s admin")
     }
     else {
+        console.log("isAdmin line 23 " + is_ad)
+        console.log("link.href 23" + link.href)
         link.href = './profile.html';
     }
 }
