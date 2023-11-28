97b480f6f66b2c2b5317e42961049d76c2f979dc
“TatyanaA” <t.alchikova@gmail.com>
2023-11-28 15:04:22 +0000

    invisible profile link if not logged in

--- assets/index.js
+++ assets/index.js
@@ -2,13 +2,15 @@ console.log('hello');
 
 
 
-function changeHref() {
+
+
+function changeHref(link_id) {
     const is_ad=localStorage.getItem("isAdmin")
 
    
    
     
-    const link = document.getElementById('link6');
+    const link = document.getElementById(link_id);
 
     console.log("link.href 13 " + link.href)
     console.log("isAdmin line 8 " + is_ad)
@@ -19,11 +21,17 @@ function changeHref() {
     link.href = "./admin.html";  
     console.log("It s admin")
     }
-    else {
+    else if(is_ad === "false") {
         console.log("isAdmin line 23 " + is_ad)
         console.log("link.href 23" + link.href)
         link.href = './profile.html';
+
+    }
+    else {
+        link.style.display = 'none';
     }
 }
-changeHref()
+changeHref('link6')
+
+module.exports = changeHref;
 
--- assets/js/register.js
+++ assets/js/register.js
@@ -25,3 +25,34 @@ document.getElementById("register-form").addEventListener("submit", async (e) =>
         alert(data.error);
     }
 })
+
+
+function changeHref(link_id) {
+    const is_ad=localStorage.getItem("isAdmin")
+
+   
+   
+    
+    const link = document.getElementById(link_id);
+
+    console.log("link.href 13 " + link.href)
+    console.log("isAdmin line 8 " + is_ad)
+    if (is_ad === "true") {
+        console.log("isAdmin line 14 " + link.href)
+
+    
+    link.href = "./admin.html";  
+    console.log("It s admin")
+    }
+    else if(is_ad === "false") {
+        console.log("isAdmin line 23 " + is_ad)
+        console.log("link.href 23" + link.href)
+        link.href = './profile.html';
+
+    }
+    else {
+        link.style.display = 'none';
+    }
+}
+
+changeHref("logout_profile_register")
