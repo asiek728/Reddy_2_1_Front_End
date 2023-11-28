f33b933e78683e33c34dfeca25793cd5a999dae9
“TatyanaA” <t.alchikova@gmail.com>
2023-11-28 13:35:20 +0000

    change for profile

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,22 @@
 console.log('hello');
+
+const is_ad=localStorage.getItem("isAdmin")
+
+console.log(is_ad)
+
+function changeHref() {
+    
+    const link = document.getElementById('link6');
+    if (is_ad==true) {
+        
+
+    
+    link.href = "./admin.html";  
+    console.log("It s admin")
+    }
+    else {
+        link.href = './profile.html';
+    }
+}
+changeHref()
+
