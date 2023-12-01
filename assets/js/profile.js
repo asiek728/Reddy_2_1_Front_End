01277ebd789af76b833b1571a06765024d721758
“TatyanaA” <t.alchikova@gmail.com>
2023-12-01 10:37:31 +0000

    fix edit profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,3 +1,7 @@
+
+function openForm() {
+    document.getElementById("edit-form").style.display = "block";
+}
 async function loadUserData() {
     const options = {
         headers: {
@@ -80,6 +84,7 @@ async function loadUserServices() {
     }
 
     const userID = localStorage.getItem("userID");
+    console.log("UserIs 87 line= ",userID)
     const response = await fetch(`http://localhost:3000/users_tasks/${userID}`, options);
 
     if (response.status == 200) {
@@ -106,6 +111,111 @@ function loginSignVisable(){
         btn.style.display = 'none';
     }
 }
+
+function openEditForm() {
+    document.getElementById("edit-form").style.display = "block";
+}
+function closeEditForm() {
+    document.getElementById("edit-form").style.display = "none";
+}
+
+function openEditFormEmail() {
+    document.getElementById("edit-form_email").style.display = "block";
+}
+function closeEditFormEmail() {
+    document.getElementById("edit-form_email").style.display = "none";
+}
+
+function editUsername() {
+const editBtn = document.getElementById("editUsername");
+    // editBtn.className = "removeBtn";
+    // editBtn.textContent = "edit"
+    console.log("line 82")
+    editBtn.addEventListener('click', async () => {
+        console.log("line 84")
+        openEditForm();
+
+        const acceptBtn = document.getElementById("accept");
+
+        acceptBtn.addEventListener('click', async () => {
+            const editUsername = document.getElementById("editStatus");
+
+            const options = {
+                method: "PATCH",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    
+                },
+                body: JSON.stringify({
+                    username: editUsername.value
+                })
+            }
+            const userID = parseInt(localStorage.getItem("userID"));
+            
+           
+            const response = await fetch(
+                `http://localhost:3000/users/username-${userID}`,
+                options
+            );
+
+            
+            console.log("Status 200 "+response.status)
+           
+
+        })
+
+    });
+
+}
+
+
+function editEmail() {
+    const editBtn = document.getElementById("editEmail");
+
+        editBtn.addEventListener('click', async () => {
+  
+            openEditFormEmail();
+    
+            const acceptBtn = document.getElementById("acceptEmail");
+    
+            acceptBtn.addEventListener('click', async () => {
+                const editEmail = document.getElementById("editEmail_value");
+    
+                const options = {
+                    method: "PATCH",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        
+                    },
+                    body: JSON.stringify({
+                        email: editEmail.value
+                    })
+                }
+                const userID = parseInt(localStorage.getItem("userID"));
+                
+               
+                const response = await fetch(
+                    `http://localhost:3000/users/email-${userID}`,
+                    options
+                );
+    
+                
+                console.log("Status 200 "+response.status)
+               
+    
+            })
+    
+        });
+    
+    }
+
+
+editUsername()
+editEmail()
+ 
+
 loginSignVisable()
 loadUserData();
 loadUserServices();
